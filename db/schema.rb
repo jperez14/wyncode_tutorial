067d2a7f91fb7de79f14785493203451e8c46430
Ed Toro <ed@wyncode.co>
2014-06-19 10:19:29 -0400

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606153001) do
+ActiveRecord::Schema.define(version: 20140609144208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "code_schools", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
+    t.integer  "code_school_id"
   end
+
+  add_index "users", ["code_school_id"], name: "index_users_on_code_school_id", using: :btree
 
 end
